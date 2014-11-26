0175cf0086af84d10ec52143a9a1085b75f8d4c9
Ravi Rani <ravirani@gmail.com>
2014-11-25 21:31:00 -0800

    Added boilerplate code for master detail view setup

---
+++ Circle/ContactTableViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  ContactTableViewCell.swift
+//  Circle
+//
+//  Created by Ravi Rani on 11/24/14.
+//  Copyright (c) 2014 RavCode. All rights reserved.
+//
+
+import UIKit
+
+class ContactTableViewCell: UITableViewCell {
+
+    @IBOutlet var profileImg: UIImageView!
+    @IBOutlet var name: UILabel!
+    @IBOutlet var title: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        profileImg.layer.cornerRadius = profileImg.frame.size.width/2.0
+        profileImg.layer.masksToBounds = true
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ Circle/UIColorExtension.swift
@@ -0,0 +1,36 @@
+//
+//  UIColorExtension.swift
+//  Circle
+//
+//  Created by Ravi Rani on 11/24/14.
+//  Copyright (c) 2014 RavCode. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    convenience init(red: Int, green: Int, blue: Int) {
+        let newRed   = CGFloat(Double(red) / 255.0)
+        let newGreen = CGFloat(Double(green) / 255.0)
+        let newBlue  = CGFloat(Double(blue) / 255.0)
+        
+        self.init(red: newRed, green: newGreen, blue: newBlue, alpha: CGFloat(1.0))
+    }
+    
+    class func appTintColor() -> UIColor {
+        return UIColor(red: 225/255.0, green: 40/255.0, blue: 47/255.0, alpha: 1.0)
+    }
+    
+    class func navigationBarBarTintColor() -> UIColor {
+        return UIColor.appTintColor()
+    }
+
+    class func navigationBarTintColor() -> UIColor {
+        return UIColor.whiteColor()
+    }
+    
+    class func navigationBarTitleColor() -> UIColor {
+        return UIColor.whiteColor()
+    }
+}
