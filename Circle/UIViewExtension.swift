f6461b0573193baa84af0742dd5e15c41a820b79
Ravi Rani <ravirani@gmail.com>
2014-11-29 13:39:52 -0800

    Add extension to provides shake animation

--- Circle/UIViewExtension.swift
+++ Circle/UIViewExtension.swift
@@ -13,4 +13,14 @@ extension UIView {
         layer.cornerRadius = 5.0
         layer.masksToBounds = true
     }
+    
+    func addShakeAnimation() {
+        var animation = CABasicAnimation(keyPath: "position")
+        animation.duration = 0.1
+        animation.repeatCount = 1
+        animation.autoreverses = true
+        animation.fromValue = NSValue(CGPoint: CGPointMake(self.center.x - 20.0, self.center.y))
+        animation.toValue = NSValue(CGPoint: CGPointMake(self.center.x + 20.0, self.center.y))
+        self.layer.addAnimation(animation, forKey: "position")
+    }
 }
