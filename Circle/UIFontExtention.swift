fec41311f3b7f91fede51a205ec2714483e5f400
Ravi Rani <ravirani@gmail.com>
2014-11-30 18:12:31 -0800

    Update nav bar titles to match the app font

---
+++ Circle/UIFontExtention.swift
@@ -0,0 +1,17 @@
+//
+//  UIFontExtention.swift
+//  Circle
+//
+//  Created by Ravi Rani on 11/30/14.
+//  Copyright (c) 2014 RavCode. All rights reserved.
+//
+
+import UIKit
+
+extension UIFont {
+
+    class func navigationBarFont() -> UIFont! {
+        return UIFont(name: "Avenir-Roman", size: 17.0)
+    }
+    
+}
