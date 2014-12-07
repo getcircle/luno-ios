6fa666583a6d7e7070f1f0352b66292ddce716e2
Ravi Rani <ravirani@gmail.com>
2014-12-06 23:49:26 -0800

    Add top menu on people list to get different groups of people
    Also, converted people to be a view controller instead of a table view controller to pin the menu to the top.

--- Circle/PeopleViewController.swift
+++ Circle/PeopleViewController.swift
@@ -8,16 +8,17 @@
 
 import UIKit
 
-class PeopleViewController: UITableViewController, MGSwipeTableCellDelegate {
+class PeopleViewController: UIViewController, MGSwipeTableCellDelegate {
 
     var profileViewController: ProfileViewController?
     var people: [Person]?
     var dataLoadAttempted: Bool!
+    @IBOutlet weak private(set) var tableView: UITableView!
+    @IBOutlet weak private(set) var topMenuSegmentedControl: UISegmentedControl!
 
     override func awakeFromNib() {
         super.awakeFromNib()
         if UIDevice.currentDevice().userInterfaceIdiom == .Pad {
-            clearsSelectionOnViewWillAppear = false
             preferredContentSize = CGSize(width: 320.0, height: 600.0)
         }
     }
@@ -28,20 +29,21 @@ class PeopleViewController: UITableViewController, MGSwipeTableCellDelegate {
         // Do any additional setup after loading the view, typically from a nib.
         dataLoadAttempted = false
         configTableView()
+        configureTopMenu()
         loadInitialData()
     }
-    
+
     override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        
+
         if dataLoadAttempted == false {
             // Checks if it has a user and loads data
             loadInitialData()
         }
     }
-    
+
     // MARK: - Configuration
-    
+
     private func configTableView() {
         // configure table view
         tableView.registerNib(
@@ -49,7 +51,17 @@ class PeopleViewController: UITableViewController, MGSwipeTableCellDelegate {
             forCellReuseIdentifier: ContactTableViewCell.classReuseIdentifier)
         tableView.separatorInset = UIEdgeInsetsMake(0.0, 64.0, 0.0, 0.0)
         tableView.rowHeight = 64.0
-        tableView.addDummyFooterView()
+    }
+
+    private func configureTopMenu() {
+        topMenuSegmentedControl.setTranslatesAutoresizingMaskIntoConstraints(false)
+        topMenuSegmentedControl.selectedSegmentIndex = 0
+        let attributes = [
+            NSFontAttributeName: UIFont.segmentedControlTitleFont()
+        ]
+
+        topMenuSegmentedControl.setTitleTextAttributes(attributes, forState: .Normal)
+        topMenuSegmentedControl.setTitleTextAttributes(attributes, forState: .Selected)
     }
 
     private func loadInitialData() {
@@ -83,15 +95,15 @@ class PeopleViewController: UITableViewController, MGSwipeTableCellDelegate {
 
     // MARK: - Table View Delegate
 
-    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
         return 1
     }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return people?.count ?? 0
     }
 
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(ContactTableViewCell.classReuseIdentifier, forIndexPath: indexPath) as ContactTableViewCell
         cell.addQuickActions = true
 
@@ -120,7 +132,7 @@ class PeopleViewController: UITableViewController, MGSwipeTableCellDelegate {
         return true
     }
 
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
         if let person = people?[indexPath.row] {
             performSegueWithIdentifier("showProfile", sender: tableView)
         }
