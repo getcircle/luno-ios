7c03e204998567c579ad65769ed08b8df46c2f3c
Ravi Rani <ravirani@gmail.com>
2014-12-24 18:11:14 -0800

    Fix lag caused by card shadows

--- Circle/SearchViewCardCollectionViewCell.swift
+++ Circle/SearchViewCardCollectionViewCell.swift
@@ -30,9 +30,11 @@ class SearchViewCardCollectionViewCell: UICollectionViewCell {
     private func customizeContainerView() {
         containerView.layer.borderWidth = 1.0
         containerView.layer.borderColor = UIColor(red: 0.8, green: 0.8, blue: 0.8, alpha: 0.3).CGColor
+        
         containerView.layer.shadowOffset = CGSizeMake(-1.0, -1.0)
         containerView.layer.shadowRadius = 4
         containerView.layer.shadowOpacity = 0.2
+        containerView.layer.shadowPath = UIBezierPath(rect: containerView.bounds).CGPath;
         containerView.layer.masksToBounds = false
     }
     
