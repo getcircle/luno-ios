efa0c92dbe8bc4c6f257346a21820ffaa026d3ba
Ravi Rani <ravirani@gmail.com>
2015-01-20 14:22:26 -0800

    Add cards back on the landing view

--- Circle/SearchLandingDataSource.swift
+++ Circle/SearchLandingDataSource.swift
@@ -13,37 +13,39 @@ class SearchLandingDataSource: CardDataSource {
     
     override func loadData(completionHandler: (error: NSError?) -> Void) {
         completionHandler(error: nil)
-//        if cards.count > 0 {
-//            return
-//        }
-//        
-//        if let currentProfile = AuthViewController.getLoggedInUserProfile() {
-//            LandingService.Actions.getCategories(currentProfile.id) { (categories, error) -> Void in
-//                if error == nil {
-//                    for category in categories ?? [] {
-//                        let categoryCard = Card(category: category)
-//                        if category.profiles.count > 0 {
-//                            var profiles = category.profiles
-//                            switch category.type {
-//                            case .Birthdays, .Anniversaries, .NewHires:
-//                                // HACK: limit the number of results in a card to 3 until we can get smarter about displaying them on the detail view
-//                                if profiles.count > 3 {
-//                                    profiles = Array(profiles[0..<3])
-//                                }
-//                            default: break
-//                            }
-//                            categoryCard.addContent(content: profiles, allContent: category.profiles)
-//                        } else if category.addresses.count > 0 {
-//                            categoryCard.addContent(content: category.addresses)
-//                        } else if category.tags.count > 0 {
-//                            categoryCard.addContent(content: category.tags)
-//                        }
-//                        self.appendCard(categoryCard)
-//                    }
-//                    completionHandler(error: nil)
-//                }
-//            }
-//        }
+        if cards.count > 0 {
+            return
+        }
+        
+        if let currentProfile = AuthViewController.getLoggedInUserProfile() {
+            LandingService.Actions.getCategories(currentProfile.id) { (categories, error) -> Void in
+                if error == nil {
+                    for category in categories ?? [] {
+                        let categoryCard = Card(category: category)
+                        if category.profiles.count > 0 {
+                            var profiles = category.profiles
+                            switch category.type {
+                            case .Birthdays, .Anniversaries, .NewHires:
+                                // HACK: limit the number of results in a card to 3 until we can get smarter about displaying them on the detail view
+                                if profiles.count > 3 {
+                                    profiles = Array(profiles[0..<3])
+                                }
+                            default: break
+                            }
+                            categoryCard.addContent(content: profiles, allContent: category.profiles)
+                        } else if category.addresses.count > 0 {
+                            categoryCard.addContent(content: category.addresses)
+                            continue
+                        } else if category.tags.count > 0 {
+                            categoryCard.addContent(content: category.tags)
+                            continue
+                        }
+                        self.appendCard(categoryCard)
+                    }
+                    completionHandler(error: nil)
+                }
+            }
+        }
     }
     
     override func configureCell(cell: CircleCollectionViewCell, atIndexPath indexPath: NSIndexPath) {
