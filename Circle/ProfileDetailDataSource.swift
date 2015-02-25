06ae2e49fb4591a1f5d69b69cbf967464edc713e
Ravi Rani <ravirani@gmail.com>
2015-02-24 16:46:38 -0800

    Add dynamic fake bios

--- Circle/ProfileDetailDataSource.swift
+++ Circle/ProfileDetailDataSource.swift
@@ -501,7 +501,21 @@ class ProfileDetailDataSource: UnderlyingCollectionViewDataSource {
         }
         else {
             // TODO: Remove after testing UI
-            card.addContent(content: [""] as [AnyObject])
+            var location = address!.name.stringByReplacingOccurrencesOfString(
+                "HQ", 
+                withString: "", 
+                options: .CaseInsensitiveSearch, 
+                range: nil
+            )
+            location = location.trimWhitespace()
+
+            let fakeBio = NSString(
+                format: "Hi! I'm %@. I work on the %@ team in %@. I am a food lover, photographer and traveler.",
+                profile.first_name,
+                team!.name,
+                location
+            )
+            card.addContent(content: [fakeBio] as [AnyObject])
         }
     }
     
