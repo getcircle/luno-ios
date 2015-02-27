475ec1e72d84a279cedd954be1593a9a2d0b1498
Ravi Rani <ravirani@gmail.com>
2015-02-26 19:00:59 -0800

    Start centralizing UI strings

---
+++ Circle/AppStrings.swift
@@ -0,0 +1,27 @@
+//
+//  AppStrings.swift
+//  Circle
+//
+//  Created by Ravi Rani on 2/26/15.
+//  Copyright (c) 2015 RH Labs Inc. All rights reserved.
+//
+
+import Foundation
+
+struct AppStrings {
+    static let QuickActionNoneLabel = ""
+    static let QuickActionEmailLabel = NSLocalizedString("Email", comment: "Title for button used to send an email")
+    static let QuickActionInfoLabel = NSLocalizedString("Info", comment: "Title for button used to see info")
+    static let QuickActionMessageLabel = NSLocalizedString("Text", comment: "Title for button used to send a message")
+    static let QuickActionNoteLabel = NSLocalizedString("Note", comment: "Title for button used to take a note")
+    static let QuickActionCallLabel = NSLocalizedString("Call", comment: "Title for button used to initiate a call")
+    static let QuickActionSlackLabel = NSLocalizedString("Slack", comment: "Title for button used send a message via Slack")
+    
+    static let QuickActionNonePlaceholder = NSLocalizedString("Search people, teams, skills, etc.", comment: "Placeholder text for search field used to search people, teams and skills.")
+    static let QuickActionEmailPlaceholder = NSLocalizedString("Who do you want to email?", comment: "Placeholder for search field used to search for the person user intends to email")
+    static let QuickActionInfoPlaceholder = NSLocalizedString("Who do you want contact info on?", comment: "Placeholder for search field used to search for the person user intends to email")
+    static let QuickActionMessagePlaceholder = NSLocalizedString("Who do you want to message?", comment: "Placeholder for search field used to search for the person user intends to send a message")
+    static let QuickActionNotePlaceholder = NSLocalizedString("Who do you want to take a note on?", comment: "Placeholder for search field used to search for the person user intends to take a note about")
+    static let QuickActionCallPlaceholder = NSLocalizedString("Who do you want to call?", comment: "Placeholder for search field used to search for the person user intends to call")
+    static let QuickActionSlackPlaceholder = NSLocalizedString("Who do you want to message?", comment: "Placeholder for search field used to search for the person user intends to send a message")
+}
