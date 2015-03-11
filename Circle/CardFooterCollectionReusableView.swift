9fad537568da9d4795a93da2f20b6ea4692de083
Ravi Rani <ravirani@gmail.com>
2015-03-10 18:13:04 -0700

    Make footer insets configurable

--- Circle/CardFooterCollectionReusableView.swift
+++ Circle/CardFooterCollectionReusableView.swift
@@ -15,6 +15,8 @@ protocol CardFooterViewDelegate {
 class CardFooterCollectionReusableView: CircleCollectionReusableView {
 
     @IBOutlet weak private(set) var footerButton: UIButton!
+    @IBOutlet weak private(set) var footerButtonLeadingConstraint: NSLayoutConstraint!
+    @IBOutlet weak private(set) var footerButtonTrailingConstraint: NSLayoutConstraint!
  
     override class var classReuseIdentifier: String {
         return "CardFooterCollectionReusableView"
@@ -30,6 +32,11 @@ class CardFooterCollectionReusableView: CircleCollectionReusableView {
         }
     }
     var cardFooterDelegate: CardFooterViewDelegate?
+    var insetEdges: Bool = true {
+        didSet {
+            updateViewForInsetEdges()
+        }
+    }
     
     override func prepareForReuse() {
         super.prepareForReuse()
@@ -42,11 +49,6 @@ class CardFooterCollectionReusableView: CircleCollectionReusableView {
         footerButton.addBottomBorder(offset: 1.0)
     }
     
-    @IBAction func footerButtonTapped(sender: AnyObject!) {
-        cardFooterDelegate?.cardFooterTapped(card)
-        setButtonTitle()
-    }
-    
     private func setButtonTitle() {
         UIView.setAnimationsEnabled(false)
         if card != nil && (card?.isContentAllContent() ?? false) {
@@ -56,5 +58,26 @@ class CardFooterCollectionReusableView: CircleCollectionReusableView {
             footerButton.setImage(UIImage(named: "Down")?.templateImage(), forState: .Normal)
         }
         UIView.setAnimationsEnabled(true)
+    }
+    
+    private func updateViewForInsetEdges() {
+        if insetEdges {
+            footerButtonLeadingConstraint.constant = 10.0
+            footerButtonTrailingConstraint.constant = 10.0
+        }
+        else {
+            footerButtonLeadingConstraint.constant = 0.0
+            footerButtonTrailingConstraint.constant = 0.0
+        }
+        
+        footerButton.setNeedsUpdateConstraints()
+        footerButton.layoutIfNeeded()
+    }
+    
+    // MARK: - IBActions
+    
+    @IBAction func footerButtonTapped(sender: AnyObject!) {
+        cardFooterDelegate?.cardFooterTapped(card)
+        setButtonTitle()
     }
 }
