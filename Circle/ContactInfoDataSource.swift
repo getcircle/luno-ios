19f918d4c006ac91a1aae022de42b37286ad1e65
Ravi Rani <ravirani@gmail.com>
2015-04-19 18:44:58 -0700

    Always show work email in contact info modal

--- Circle/ContactInfoDataSource.swift
+++ Circle/ContactInfoDataSource.swift
@@ -22,6 +22,18 @@ class ContactInfoDataSource: CardDataSource {
         let card = Card(cardType: .KeyValue, title: "Info")
         card.sectionInset = UIEdgeInsetsMake(0.0, 0.0, 1.0, 0.0)
         card.showContentCount = false
+        
+        var content = [[String: AnyObject]]()
+        
+        // Add explicit row for Work Email
+        let emailKeyValueDict: [String: AnyObject] = [
+            "key": "work_email",
+            "name": "Email",
+            "value": profile.email,
+            "type": Int(Services.Profile.Containers.ContactMethodV1.ContactMethodTypeV1.Email.rawValue)
+        ]
+        content.append(emailKeyValueDict)
+        
         for contactMethod in profile.contactMethods {
             var dataDict: [String: AnyObject] = [
                 "key": contactMethod.label,
@@ -29,14 +41,18 @@ class ContactInfoDataSource: CardDataSource {
                 "value": contactMethod.value,
                 "type": Int(contactMethod.contactMethodType.rawValue),
             ]
-            card.addContent(content: [dataDict])
+            content.append(dataDict)
+        }
+        
+        if content.count > 0 {
+            card.addContent(content: content)
         }
         appendCard(card)
         completionHandler(error: nil)
     }
     
     func heightOfContactInfoSection() -> CGFloat {
-        return KeyValueCollectionViewCell.height * CGFloat(profile.contactMethods.count)
+        return KeyValueCollectionViewCell.height * CGFloat((profile.contactMethods.count + 1))
     }
     
 }
