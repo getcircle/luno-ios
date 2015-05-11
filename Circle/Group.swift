1aadb6e9d893bfa40648dc8783284c8ded3d7366
Michael Hahn <mwhahn@gmail.com>
2015-05-10 18:17:25 -0700

    Remove stubbed out service call

--- Circle/Group.swift
+++ Circle/Group.swift
@@ -15,6 +15,7 @@ typealias GetGroupProfilesCompletionHandler = (
     error: NSError?
 ) -> Void
 
+
 extension Services.Group.Actions {
     
     static func listMembers(
@@ -23,46 +24,24 @@ extension Services.Group.Actions {
         paginatorBuilder: Soa.PaginatorV1Builder? = nil,
         completionHandler: GetGroupProfilesCompletionHandler?
     ) {
+        let requestBuilder = Services.Group.Actions.ListMembers.RequestV1.builder()
+        requestBuilder.groupId = groupId
+        requestBuilder.role = role
+        requestBuilder.provider = .Google
         
-            // TODO: REMOVE WHEN BACKEND IS READY
-            //===================================================================================
-                completionHandler?(members: getFakeMembers(), nextRequest: nil, error: nil)
-                return
-            //===================================================================================
-        
-            let requestBuilder = Services.Group.Actions.ListMembers.RequestV1.builder()
-            requestBuilder.groupId = groupId
-            requestBuilder.role = role
-            
-            let client = ServiceClient(serviceName: "group")
-            client.callAction("list_members",
-                extensionField: Services.Registry.Requests.Group.listMembers(),
-                requestBuilder: requestBuilder,
-                paginatorBuilder: paginatorBuilder
-            ){
-                (_, _, wrapped, error) -> Void in
-                let response = wrapped?.response?.result.getExtension(
-                    Services.Registry.Responses.Group.listMembers()
-                ) as? Services.Group.Actions.ListMembers.ResponseV1
-                let nextRequest = wrapped?.getNextRequest()
-                completionHandler?(members: response?.members, nextRequest: nextRequest, error: error)
-            }
+        let client = ServiceClient(serviceName: "group")
+        client.callAction("list_members",
+            extensionField: Services.Registry.Requests.Group.listMembers(),
+            requestBuilder: requestBuilder,
+            paginatorBuilder: paginatorBuilder
+        ){
+            (_, _, wrapped, error) -> Void in
+            let response = wrapped?.response?.result.getExtension(
+                Services.Registry.Responses.Group.listMembers()
+            ) as? Services.Group.Actions.ListMembers.ResponseV1
+            let nextRequest = wrapped?.getNextRequest()
+            completionHandler?(members: response?.members, nextRequest: nextRequest, error: error)
+        }
     }
     
-    // TODO: REMOVE WHEN BACKEND IS READY
-    private static func getFakeMembers() -> Array<Services.Group.Containers.MemberV1> {
-        var members = Array<Services.Group.Containers.MemberV1>()
-        for i in 0..<6 {
-            let member = Services.Group.Containers.MemberV1.builder()
-            let profile = Services.Profile.Containers.ProfileV1.builder()
-            profile.id = "hello"
-            profile.title = "Co-founder"
-            profile.fullName = "Ravi Rani"
-            profile.firstName = "Ravi"
-            profile.lastName = "Rani"
-            member.profile = profile.build()
-            members.append(member.build())
-        }
-        return members
-    }
 }
