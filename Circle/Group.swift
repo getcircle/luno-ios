0965a844f25e9d618d19a7f93e9273147d52c32b
Ravi Rani <ravirani@gmail.com>
2015-05-26 12:07:18 -0700

    Add leave group & respond to membership actions

--- Circle/Group.swift
+++ Circle/Group.swift
@@ -26,6 +26,16 @@ typealias JoinGroupCompletionHandler = (
     error: NSError?
 ) -> Void
 
+typealias LeaveGroupCompletionHandler = (
+    status: Services.Group.Containers.MembershipRequestStatusV1?,
+    error: NSError?
+) -> Void
+
+typealias RespondToMembershipRequestCompletionHandler = (
+    status: Services.Group.Containers.MembershipRequestStatusV1?,
+    error: NSError?
+) -> Void
+
 
 extension Services.Group.Actions {
     
@@ -94,6 +104,51 @@ extension Services.Group.Actions {
                     Services.Registry.Responses.Group.joinGroup()
                 ) as? Services.Group.Actions.JoinGroup.ResponseV1
                 completionHandler?(status: response?.request.status, error: error)
+            }
+    }
+
+    static func leaveGroup(
+        groupId: String,
+        completionHandler: LeaveGroupCompletionHandler?
+    ) {
+            let requestBuilder = Services.Group.Actions.LeaveGroup.RequestV1.builder()
+            requestBuilder.groupKey = groupId
+            
+            let client = ServiceClient(serviceName: "group")
+            client.callAction("leave_group",
+                extensionField: Services.Registry.Requests.Group.leaveGroup(),
+                requestBuilder: requestBuilder,
+                paginatorBuilder: nil
+            ){
+                (_, _, wrapped, error) -> Void in
+                let response = wrapped?.response?.result.getExtension(
+                    Services.Registry.Responses.Group.leaveGroup()
+                ) as? Services.Group.Actions.LeaveGroup.ResponseV1
+                // completionHandler?(status: response?.request.status, error: error)
+            }
+    }
+    
+    static func respondToMembershipRequest(
+        requestId: String,
+        action: Services.Group.Actions.RespondToMembershipRequest.RequestV1.ResponseActionV1,
+        completionHandler: RespondToMembershipRequestCompletionHandler?
+    ) {
+            let requestBuilder = Services.Group.Actions.RespondToMembershipRequest.RequestV1.builder()
+            requestBuilder.action = action
+            requestBuilder.requestId = requestId
+            
+            let client = ServiceClient(serviceName: "group")
+            client.callAction("respond_to_membership_request",
+                extensionField: Services.Registry.Requests.Group.respondToMembershipRequest(),
+                requestBuilder: requestBuilder,
+                paginatorBuilder: nil
+            ){
+                (_, _, wrapped, error) -> Void in
+                let response = wrapped?.response?.result.getExtension(
+                    Services.Registry.Responses.Group.respondToMembershipRequest()
+                ) as? Services.Group.Actions.RespondToMembershipRequest.ResponseV1
+                
+                // completionHandler?(status: response?.request.status, error: error)
             }
     }
 }
