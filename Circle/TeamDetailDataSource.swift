54b84b9e714cd1f9e5a1c478dc94103df050b9ca
Felix Mo <felix@felixmo.com>
2015-09-11 15:51:24 -0400

    Add appCardContentSeparatorViewColor to UIColorExtension

--- Circle/TeamDetailDataSource.swift
+++ Circle/TeamDetailDataSource.swift
@@ -308,7 +308,7 @@ class TeamDetailDataSource: CardDataSource {
                 let isLastCell = (indexPath.row == card.content.count - 1)
                 
                 cell.separatorInset = UIEdgeInsetsMake(0.0, 20.0, 0.0, 0.0)
-                cell.separatorColor = UIColor.blackColor().colorWithAlphaComponent(0.06)
+                cell.separatorColor = UIColor.appCardContentSeparatorViewColor()
                 cell.showSeparator = !(isLastCell && !card.addFooter)
             }
         }
