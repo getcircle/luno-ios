8029b266ef4ff7606146613f54d157cad36b82c0
Felix Mo <felix@felixmo.com>
2015-10-09 18:55:05 -0400

    Update EditProfileStatusViewController for change to saveData:

--- Circle/EditProfileStatusViewController.swift
+++ Circle/EditProfileStatusViewController.swift
@@ -39,29 +39,24 @@ class EditProfileStatusViewController: TextInputViewController {
         return "What are you working on?"
     }
     
-    override func saveData(data: String) {
-        do {
-            let statusBuilder: Services.Profile.Containers.ProfileStatusV1.Builder
-            if let status = profile.status {
-                statusBuilder = try status.toBuilder()
-            }
-            else {
-                statusBuilder = Services.Profile.Containers.ProfileStatusV1.Builder()
-            }
-            statusBuilder.value = data
-            
-            let profileBuilder = try profile.toBuilder()
-            profileBuilder.status = try statusBuilder.build()
-            Services.Profile.Actions.updateProfile(try profileBuilder.build()) { (profile, error) -> Void in
-                if let profile = profile {
-                    AuthenticationViewController.updateUserProfile(profile)
-                }
-                
-                self.onDataSaved(profile)
-            }
+    override func saveData(data: String) throws {
+        let statusBuilder: Services.Profile.Containers.ProfileStatusV1.Builder
+        if let status = profile.status {
+            statusBuilder = try status.toBuilder()
         }
-        catch {
-            print("Error: \(error)")
+        else {
+            statusBuilder = Services.Profile.Containers.ProfileStatusV1.Builder()
+        }
+        statusBuilder.value = data
+        
+        let profileBuilder = try profile.toBuilder()
+        profileBuilder.status = try statusBuilder.build()
+        Services.Profile.Actions.updateProfile(try profileBuilder.build()) { (profile, error) -> Void in
+            if let profile = profile {
+                AuthenticationViewController.updateUserProfile(profile)
+            }
+            
+            self.onDataSaved(profile)
         }
     }
 }
