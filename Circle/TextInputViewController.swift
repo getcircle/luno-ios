de05fbf35f8b2686499825b6fc55e27dbb77cb7f
Ravi Rani <ravirani@gmail.com>
2015-10-11 19:26:02 -0700

    Don't go to Notifications after adding a status

--- Circle/TextInputViewController.swift
+++ Circle/TextInputViewController.swift
@@ -264,18 +264,6 @@ class TextInputViewController: UIViewController, UITextViewDelegate {
         }
         
         delegate?.onTextInputValueUpdated(updatedObject)
-        if addPostButton {
-            goToNextVC()
-        }
-        else {
-            dismissView()
-        }
-    }
-    
-    // MARK: - IBActions
-    
-    private func goToNextVC() {
-        let notificationsVC = NotificationsViewController(nibName: "NotificationsViewController", bundle: nil)
-        navigationController?.pushViewController(notificationsVC, animated: true)
+        dismissView()
     }
 }
