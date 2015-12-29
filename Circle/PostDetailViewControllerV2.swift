168de85e3d6de90a19e73843a1a5e00703ef0b4b
Ravi Rani <ravirani@gmail.com>
2015-12-28 20:19:57 -0800

    Add support for previewing common file types
    We use to support images but the work to add office and iwork documents was trivial.

--- Circle/PostDetailViewControllerV2.swift
+++ Circle/PostDetailViewControllerV2.swift
@@ -8,9 +8,14 @@
 
 import UIKit
 import WebKit
+import Alamofire
+import MBProgressHUD
 import ProtobufRegistry
 
-class PostDetailViewControllerV2: UIViewController, UIScrollViewDelegate, WKNavigationDelegate {
+class PostDetailViewControllerV2: UIViewController,
+UIScrollViewDelegate,
+UIDocumentInteractionControllerDelegate,
+WKNavigationDelegate {
     
     @IBOutlet weak private(set) var authorButton: UIButton!
     @IBOutlet weak private(set) var authorLabel: UILabel!
@@ -96,25 +101,6 @@ class PostDetailViewControllerV2: UIViewController, UIScrollViewDelegate, WKNavi
         }
     }
     
-    // MARK: - Helpers
-    
-    private func getTemplateString() -> String? {
-        if self.dynamicType.templateString == nil {
-            do {
-                print("Fetching template")
-                if let templateFile = NSBundle.mainBundle().pathForResource("PostTemplate", ofType: "html") {
-                    let htmlTemplate = try NSString(contentsOfFile: templateFile, encoding: 4)
-                    self.dynamicType.templateString = htmlTemplate as String
-                }
-            }
-            catch {
-                print("Error: \(error)")
-            }
-        }
-        
-        return self.dynamicType.templateString
-    }
-    
     // MARK: - Data Source
     
     private func loadPost() {
@@ -178,5 +164,137 @@ class PostDetailViewControllerV2: UIViewController, UIScrollViewDelegate, WKNavi
         if let activityIndicatorView = activityIndicatorView {
             activityIndicatorView.stopAnimating()
         }
+    }
+    
+    func webView(webView: WKWebView, decidePolicyForNavigationAction navigationAction: WKNavigationAction, decisionHandler: (WKNavigationActionPolicy) -> Void) {
+
+        if let url = navigationAction.request.URL {
+            
+            if url.absoluteString == "about:blank" {
+                // Allow initial load
+                decisionHandler(.Allow)
+                return
+            }
+            else if navigationAction.navigationType == .LinkActivated {
+                // For link clicks
+                // If supported file type, go preview it
+                // If not supported file type, go to SafariVC on iOS 9 or actual browser on lower versions
+                
+                if canPreviewURL(url) {
+                    // Download and Preview
+                    downloadAndPreviewURL(url)
+                }
+                else {
+                    // Go to the browser
+                    openURLInBrowser(url)
+                }
+            }
+        }
+
+        decisionHandler(.Cancel)
+    }
+    
+    // MARK: - UIDocumentInteractionControllerDelegate
+    
+    func documentInteractionControllerViewControllerForPreview(controller: UIDocumentInteractionController) -> UIViewController {
+        return self
+    }
+    
+    func documentInteractionControllerDidEndPreview(controller: UIDocumentInteractionController) {
+        if let tempFileUrl = controller.URL {
+            do {
+                try NSFileManager.defaultManager().removeItemAtURL(tempFileUrl)
+            }
+            catch {
+                print("Error: \(error)")
+            }
+        }
+    }
+    
+    // MARK: - Helpers
+    
+    private func getTemplateString() -> String? {
+        if self.dynamicType.templateString == nil {
+            do {
+                print("Fetching template")
+                if let templateFile = NSBundle.mainBundle().pathForResource("PostTemplate", ofType: "html") {
+                    let htmlTemplate = try NSString(contentsOfFile: templateFile, encoding: 4)
+                    self.dynamicType.templateString = htmlTemplate as String
+                }
+            }
+            catch {
+                print("Error: \(error)")
+            }
+        }
+        
+        return self.dynamicType.templateString
+    }
+    
+    // For rich text posts, all attachments come as part of HTML
+    // and there isn't a clean way (currently we can inject some javascript and read data-*
+    // attributes to determine type and notify VC) to access them separately and check their types.
+    // WKWebView also doesn't provide info on where a click happened.
+    // It only gets a delegate callback with the actual URL.
+    // The following code conservatively checks for popular file extensions
+    // that we can preview. The full list is available here:
+    // https://developer.apple.com/library/ios/documentation/NetworkingInternet/Reference/QLPreviewController_Class/index.html#//apple_ref/occ/cl/QLPreviewController
+    private func canPreviewURL(url: NSURL) -> Bool {
+        let supportedExtensions: Set<String> = [
+            // Images
+            "png", "jpeg", "jpg", "gif", "svg",
+
+            // Text Documents
+            "text", "txt", "rtf", "csv",
+            
+            // iWork
+            "key", "pages", "numbers",
+            
+            // Office
+            "docx", "doc", "xlsx", "xls", "ppt", "pptx",
+            
+            // PDF
+            "pdf",
+        ]
+        
+        if let fileExtension = url.pathExtension where supportedExtensions.contains(fileExtension)  {
+            print(fileExtension)
+            return true
+        }
+        
+        return false
+    }
+
+    private func downloadAndPreviewURL(url: NSURL) {
+        let hud = MBProgressHUD.showHUDAddedTo(view, animated: true)
+        Alamofire.request(.GET, url).response(completionHandler: { (request, response, data, error) -> Void in
+            hud.hide(true)
+            if let error = error {
+                print("Error: \(error)")
+            }
+            else if let data = data {
+                self.previewFileData(data, fileName: url.lastPathComponent)
+            }
+        })
+    }
+    
+    private func previewFileData(data: NSData, fileName: String?) {
+        if let fileName = fileName {
+            // Write attachment data to a temp file so we can preview it
+            let tempFileUrl = NSURL(fileURLWithPath: NSTemporaryDirectory(), isDirectory: true)
+                .URLByAppendingPathComponent(fileName)
+            do {
+                try data.writeToURL(tempFileUrl, options: [])
+                let documentInteractionController = UIDocumentInteractionController(URL: tempFileUrl)
+                documentInteractionController.delegate = self
+                documentInteractionController.presentPreviewAnimated(true)
+            }
+            catch {
+                print("Error: \(error)")
+            }
+        }
+    }
+    
+    private func openURLInBrowser(url: NSURL) {
+        UIApplication.sharedApplication().openURL(url)
     }
 }
