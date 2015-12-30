4f305bf1bf4630ad1176220941d15ed3c0d6f1fe
Ravi Rani <ravirani@gmail.com>
2015-12-30 00:18:38 -0800

    Update version & point to dev

--- Circle/ServiceTransport.swift
+++ Circle/ServiceTransport.swift
@@ -187,8 +187,8 @@ struct ServiceHttpRequest: URLRequestConvertible {
     }
     
 //    static let environment = Environment.Production
-    static let environment = Environment.Local
-//    static let environment = Environment.Dev
+//    static let environment = Environment.Local
+    static let environment = Environment.Dev
     
     var data: NSData
     var token: String?
